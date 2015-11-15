5b3a280d1c0f5ae3b962606d1c53054fc0b58ef3
Michael Soohoo <mbs729@gmail.com>
2015-11-15 15:04:59 -0800

    Added iterative binary search

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,5 @@
 class BinarySearch {
-  public static int BinarySearch(int[] arr, int val, int left, int right) {
+  public static int binarySearch(int[] arr, int val, int left, int right) {
     int mid = (left + right) / 2;
 
     if (left > right) {
@@ -7,17 +7,33 @@ class BinarySearch {
     }
 
     if (val < arr[mid]) {
-      return BinarySearch(arr, val, left, mid-1);
+      return binarySearch(arr, val, left, mid-1);
     } else if (val > arr[mid]) {
-      return BinarySearch(arr, val, mid+1, right);
+      return binarySearch(arr, val, mid+1, right);
     } else {
       return mid;
     }
   }
 
+  public static int binarySearchIter(int[] arr, int val, int left, int right) {
+    while (left <= right) {
+      int mid = (left + right) / 2;
+      if (val < arr[mid]) {
+        right = mid-1;
+      } else if (val > arr[mid]) {
+        left = mid+1;
+      } else {
+        return mid;
+      }
+    }
+    return -1;
+  }
+
   public static void main(String[] args) {
     int[] nums = {1, 3, 5, 6, 7, 9, 11, 15};
-    System.out.println(BinarySearch(nums, 11, 0, nums.length-1) == 6);
-    System.out.println(BinarySearch(nums, 8, 0, nums.length-1) == -1);
+    System.out.println(binarySearch(nums, 11, 0, nums.length-1) == 6);
+    System.out.println(binarySearchIter(nums, 11, 0, nums.length-1) == 6);
+    System.out.println(binarySearch(nums, 8, 0, nums.length-1) == -1);
+    System.out.println(binarySearchIter(nums, 8, 0, nums.length-1) == -1);
   }
 }
